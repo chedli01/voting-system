d40a101caef1ca277ca5fb8cfd0170b07383ce52
Kousay Jebir <kousayjebir@gmail.com>
2024-11-11 10:34:43 +0100

    fix import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import GlobalLayout from "./GlobalLayout";
-import HomePage from "./HomePage";
+import GlobalLayout from "./shared/components/app-layout/GlobalLayout";
+import HomePage from "./pages/home/HomePage"
 import "./app.css"
-import RegisterPage from "./RegisterPage";
+import RegisterPage from "./pages/register/RegisterPage"
 import Error404Page from "./pages/error/Error404Page"
 function App() {
 
