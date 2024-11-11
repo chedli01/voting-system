bc8636140956b5787d4754b4afb2c9135a94d143
Kousay Jebir <kousayjebir@gmail.com>
2024-11-11 11:51:33 +0100

    fix

--- client/src/pages/register/RegisterPage.jsx
+++ client/src/pages/register/RegisterPage.jsx
@@ -15,7 +15,7 @@ export default function RegisterPage(){
             try{
                 const result = await checkConnection();
                 console.log(result);
-                if(result.data.connected){
+                if(result.connected){
                     navigate("/home")
                 }
             }
