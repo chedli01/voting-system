e7417c5e63e849d2b15f2fbd3d4f9f6afd5e48a5
Kousay Jebir <kousayjebir@gmail.com>
2024-11-11 11:27:01 +0100

    testing reigster endpoint

--- client/src/pages/register/RegisterPage.jsx
+++ client/src/pages/register/RegisterPage.jsx
@@ -1,8 +1,24 @@
 import "./RegisterPage.css"
 import enter from "../../assets/enter.png"
 import jeiLogo from "../../assets/jei2.png"
+import { useState } from "react"
+import { registerUser } from "../../service/api";
 
 export default function RegisterPage(){
+
+    const [userInput,setUserInput] = useState("");
+
+    const submitHandler = async (event)=>{
+        event.preventDefault();
+        try{
+        const result = await registerUser(userInput);
+        console.log(result);
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
     return(
         <div className="register-form-container">
             <img src={jeiLogo} alt="JEILogo" width="100px" height="50px"/>
@@ -11,8 +27,8 @@ export default function RegisterPage(){
                 <h1 className="title-2">3.0</h1>
             </div>
             <h2 style={{fontSize:'1em', color:'rgba(255,255,255,0.55)'}}>Voting System</h2>
-            <form >
-                <input type="text" name="unique-code" id="unique-code" placeholder="Enter your code" />
+            <form onSubmit={submitHandler}>
+                <input type="text" name="unique-code" id="unique-code" placeholder="Enter your code" value={userInput} onChange={(e)=>{setUserInput(e.target.value)}}/>
                 <button id="register-form-submit" type="submit"><img src={enter} width='24px' height='30px'/></button>
             </form>
         </div>
