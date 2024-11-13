ecf91e93e86ff7d2fee83e4b00fc0f94414f6183
Kousay Jebir <kousayjebir@gmail.com>
2024-11-13 14:43:27 +0100

    hopefully this works now

--- server/src/check/status.mjs
+++ server/src/check/status.mjs
@@ -2,11 +2,10 @@ import { Router } from "express";
 const route = Router();
 
 route.get("/api/isconnected",async(req,res)=>{
-    /* if(req.cookies.connectionCookie) {return res.json({connected:true})}
+    if(req.cookies.connectionCookie) {return res.json({connected:true})}
     else{
         return res.json({connected:false})
-    } */
-   return res.json({connected:true})
+    }
 })
 
 export default route;
--- server/src/mongodb/connect.mjs
+++ server/src/mongodb/connect.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function dbconfig() {
   await mongoose
-    .connect("mongodb://127.0.0.1:27017/votingsystem?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3")
+    .connect("mongodb://127.0.0.1:27017/votingsystem?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3&replicaSet=rs0")
     .then(() =>console.log("connected to db"))
     .catch((err) => console.log(err));
 }
