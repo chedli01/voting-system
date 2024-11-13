916977a3cc332f9b55e49f6efd08a21987c61ccf
Kousay Jebir <kousayjebir@gmail.com>
2024-11-13 11:52:38 +0100

    d

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -16,10 +16,10 @@ import hasVotedRouter from "./check/hasvoted.mjs"
 /* import fs from 'fs';
 import https from 'https' */
 
-const sslOptions = {
+/* const sslOptions = {
   cert: fs.readFileSync('/home/juniorentreprise/ssl/certs/voting_jeinsat_com_b3467_4e76f_1739204081_2e382c7909bbf6759c8663fc06424aac.crt'),
   key: fs.readFileSync('/home/juniorentreprise/ssl/keys/b3467_4e76f_03f374f85221b3c8addbb5fdbe6fd4c9.key'),
-};
+}; */
 
 
 const app = express();
