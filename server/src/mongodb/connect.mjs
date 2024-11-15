0aa642cc5f2291972f8b914f913e19a626e31fe3
Kousay Jebir <kousayjebir@gmail.com>
2024-11-15 04:52:01 +0100

    it finally worked fully dockerized

--- server/src/mongodb/connect.mjs
+++ server/src/mongodb/connect.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function dbconfig(connectionString) {
   await mongoose
-    .connect(`mongodb://mongo:27017/votingsystem?directConnection=true?replicaSet=rs0`,{serverSelectionTimeoutMS: 5000,directConnection:true})
+    .connect(`${connectionString}`,{serverSelectionTimeoutMS: 5000,directConnection:true})
     .then(() =>console.log("connected to db"))
     .catch((err) => console.log(err));
 }
--- server/src/vote/sendvote.mjs
+++ server/src/vote/sendvote.mjs
@@ -10,6 +10,7 @@ route.get("/api/sendvote", async (req, res) => {
       res.setHeader("Content-Type", "text/event-stream");
       res.setHeader("Cache-Control", "no-cache");
       res.setHeader("Connection", "keep-alive");
+      res.setHeader("X-Accel-Buffering", "no");  // Disable buffering for SSE
 
       // Watch for changes in the CurrentVote collection
       const changeStream = CurrentVote.watch([], { fullDocument: "updateLookup" });
