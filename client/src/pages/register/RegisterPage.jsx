c7ecbb6193deaa9d3ca5560e3f0acf7b5b4070ea
Kousay Jebir <kousayjebir@gmail.com>
2024-11-17 15:32:41 +0100

    fix css

--- client/src/pages/register/RegisterPage.jsx
+++ client/src/pages/register/RegisterPage.jsx
@@ -47,7 +47,7 @@ export default function RegisterPage(){
                 <h1 className="title-2">3.0</h1>
             </div>
             <h2 style={{fontSize:'1em', color:'rgba(255,255,255,0.55)'}}>Voting System</h2>
-            <form onSubmit={submitHandler} style={{display:'relative'}}>
+            <form onSubmit={submitHandler} style={{position:'relative'}}>
                 <input type="text" name="unique-code" id="unique-code" placeholder="Enter your code" value={userInput} onChange={(e)=>{setUserInput(e.target.value)}}/>
                 <button id="register-form-submit" type="submit"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 14a1 1 0 0 1 1-1h12a3 3 0 0 0 3-3V6a1 1 0 1 1 2 0v4a5 5 0 0 1-5 5H4a1 1 0 0 1-1-1z" fill="#ffffff"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M3.293 14.707a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 1.414L5.414 14l3.293 3.293a1 1 0 1 1-1.414 1.414l-4-4z" fill="#ffffff"></path></g></svg></button>
             </form>
