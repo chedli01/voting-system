22dd867511717825fd92cff412f2255ecbc70ae2
Kousay Jebir <kousayjebir@gmail.com>
2024-11-19 11:43:05 +0100

    d

--- server/src/vote/voting.mjs
+++ server/src/vote/voting.mjs
@@ -11,8 +11,6 @@ route.post("/api/vote/:id",async(req,res)=>{
     const currentvote=await CurrentVote.find();
     const id=currentvote[0].teamID  */
     if(req.cookies.connectionCookie /* && teamId==id  */){
-
-            const rawCookie = req.cookies.connectionCookie.j; 
     /*     const user=await Voter.findOne({code:req.cookies.connectionCookie.code});
         const exist=user.votes.find(element => element == teamId)!== undefined;
          */
@@ -32,13 +30,11 @@ route.post("/api/vote/:id",async(req,res)=>{
                
                 await Team.updateOne({id:id},{$set:{score:score}});
             } */
-           const code = JSON.parse(rawCookie);
-           const parsedCode = code.code
-           await Voter.updateOne({code:parsedCode},{$push:{votes:parseInt(teamId)}});
+           await Voter.updateOne({code:req.cookies.connectionCookie.code},{$push:{votes:parseInt(teamId)}});
 
            
             
-            return res.status(201).json({voted:true,code:parsedCode})
+            return res.status(201).json({voted:true,code:req.cookies.connectionCookie})
         }
        
         else{
