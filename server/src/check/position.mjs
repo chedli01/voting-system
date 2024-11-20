da225998b3b1a462045e84408732987e21e6c7d0
Kousay Jebir <kousayjebir@gmail.com>
2024-11-20 13:22:59 +0100

    back to audito radius

--- server/src/check/position.mjs
+++ server/src/check/position.mjs
@@ -24,8 +24,7 @@ route.post("/api/verifyPosition",async(req,res)=>{
     const accuracy=req.body.accuracy
     /* const radius = 10; */
     //high radius for testing purposes
-    /* const radius = 40; */
-    const radius =3000
+    const radius = 40;
     if(accuracy>eventAccuracy){
         adjust+=(accuracy-eventAccuracy)
     }
