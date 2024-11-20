3810a4ffcf0546101ab8be1adbb78dec04099ba2
Kousay Jebir <kousayjebir@gmail.com>
2024-11-20 14:12:20 +0100

    more radius

--- server/src/check/position.mjs
+++ server/src/check/position.mjs
@@ -24,7 +24,7 @@ route.post("/api/verifyPosition",async(req,res)=>{
     const accuracy=req.body.accuracy
     /* const radius = 10; */
     //high radius for testing purposes
-    const radius = 40;
+    const radius = 50;
     if(accuracy>eventAccuracy){
         adjust+=(accuracy-eventAccuracy)
     }
